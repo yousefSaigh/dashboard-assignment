e5f0d23ff0d84859a95b5ad116d17bf0f46a428c
yousefSaigh <46692129+yousefSaigh@users.noreply.github.com>
2021-08-20 13:11:59 -0500

    fix logger

--- dashboard-app/src/module/store.ts
+++ dashboard-app/src/module/store.ts
@@ -3,16 +3,22 @@ import { ISagaModule } from 'redux-dynamic-modules-saga';
 import { getSagaExtension } from 'redux-dynamic-modules-saga';
 import { getThunkExtension } from 'redux-dynamic-modules-thunk';
 import { applyMiddleware } from 'redux';
-import { logger } from 'redux-logger';
+import { createLogger, logger } from 'redux-logger';
+import { getUsersModule } from '.';
+
+const customLogger = createLogger({});
 
 export const rootModule: ISagaModule<any> = {
     id: 'initial',
     middlewares: [logger],
 };
 
-const store: IModuleStore<any> = createStore({
-    enhancers: [applyMiddleware()],
-    extensions: [getSagaExtension(), getThunkExtension()],
-});
+const store: IModuleStore<any> = createStore(
+    {
+        enhancers: [applyMiddleware(customLogger)],
+        extensions: [getSagaExtension(), getThunkExtension()],
+    },
+    getUsersModule,
+);
 
 export default store;
